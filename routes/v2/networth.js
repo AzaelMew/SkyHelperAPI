b8ec038ef5e0558cfba18f7cce4d1a194eacd9e0
AzaelMew <dogydemon@gmail.com>
2023-08-06 20:17:39 +0300

    fuck all of this.

--- routes/v2/networth.js
+++ routes/v2/networth.js
@@ -7,7 +7,6 @@ module.exports = wrap(async function (req, res) {
     const data = req.body;
     if (!data) res.status(400).json({ status: 400, reason: 'Missing request body' });
     if (!data?.profileData) res.status(400).json({ status: 400, reason: 'Missing "profileData" in request body' });
-
     const networth = await getNetworth(data.profileData, data.bankBalance, { prices: getPrices(), onlyNetworth: data.onlyNetworth });
 
     return res.status(200).json({ status: 200, data: networth });
